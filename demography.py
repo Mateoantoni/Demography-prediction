834a75a7604df66fb84300b5ec81acbe401b524e
Matéo <mateo.antoni-christ@epitech.eu>
2025-01-21 15:36:02 +0100

    Fix: giving correct result for ax + b and ay + b

--- demography.py
+++ demography.py
@@ -12,21 +12,20 @@ def parser() -> list[dict]:
     return buffer
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <Country Code>")
+        return 84
+    
     buffer = parser()
     for row in buffer:
         if row["Country Code"] == sys.argv[1]:
-            print(row)
             print("Country:", row["Country Name"])
             x = [int(i) for i in list(row.values())[2:]]
             y = [int(i) for i in list(row.keys())[2:]]
-            print(x)
-            print(y)
-
-    n = 57
-    x_list = x[2:]
-    y_list = y[2:]
+    x_list = y
+    y_list = x
+    n = len(x_list)
     
-
     sum_X = sum(x_list)
     sum_Y = sum(y_list)
     sum_XX = sum(x**2 for x in x_list)
@@ -34,20 +33,22 @@ def main():
     sum_XY = sum(x * y for x, y in zip(x_list, y_list))
 
     # For Fit 1 (Y = aX * X + bX)
-    aX = (n * sum_XY - sum_X * sum_Y) / (n * sum_XX - sum_X**2)
-    bX = (sum_Y - aX * sum_X) / n
+    aX = (n * sum_XY - sum_X * sum_Y) / (n * sum_XX - sum_X * sum_X)
+    bX = (sum_Y * sum_XX - sum_X * sum_XY) / (n * sum_XX - sum_X * sum_X)
+    aX = aX / 1000000
+    bX = bX / 1000000
+
 
     # For Fit 2 (X = aY * Y + bY)
-    aY = (n * sum_XY - sum_Y * sum_X) / (n * sum_YY - sum_Y**2)
-    aY = aY / 1000000
-    bY = (sum_X - aY * sum_Y) / n
-    bY = bY / 1000000
+    aY = (n * sum_XY - sum_X * sum_Y) / (n * sum_YY - sum_Y * sum_Y)
+    bY = (sum_X * sum_YY - sum_Y * sum_XY) / (n * sum_YY - sum_Y * sum_Y)
+    aY = aY * 1000000
 
-    Y_pred = []* n
-    for x in x_list:
-        Y_pred.append(x*aX + bX)
+    Y_pred = [aX * x + bX for x in x_list]
+    RMSD_x = math.sqrt(sum((y - y_pred)**2 for y, y_pred in zip(y_list, Y_pred)) / n)
 
-    RMSD = math.sqrt(sum((sum_Y - y)** 2 for y in Y_pred)) / n
+    X_pred = [aY * y + bY for y in y_list]
+    RMSD_y = math.sqrt(sum((x - x_pred)**2 for x, x_pred in zip(x_list, X_pred)) / n)
 
     print("Fit 1")
     if bX < 0:
@@ -55,7 +56,7 @@ def main():
         print("Y =", "{:.2f}".format(aX), "X -", "{:.2f}".format(bX))
     else:
         print("Y =", "{:.2f}".format(aX), "X +", "{:.2f}".format(bX))
-    print("Root-mean-square deviation:", "{:.2f}".format(RMSD))
+    print("Root-mean-square deviation:", "{:.2f}".format(RMSD_x))
     print("Population in 2050:", "nb en m")
 
     print("Fit 2")
@@ -64,7 +65,7 @@ def main():
         print("X =", "{:.2f}".format(aY), "Y", "-", "{:.2f}".format(bY))
     else:
         print("X =", "{:.2f}".format(aY), "Y", "+", "{:.2f}".format(bY))
-    print("Root-mean-square deviation:", "{:.2f}".format(RMSD))
+    print("Root-mean-square deviation:", "{:.2f}".format(RMSD_y))
     print("Population in 2050:", "nb en m")
 if __name__ == "__main__":
     main()
